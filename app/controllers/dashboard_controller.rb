a27186277f17dfa03496b94b6c60d42c7f5d5600
Ezekiel <ezerivadeneiral@gmail.com>
2015-06-27 00:12:15 -0300

    Add title and subtitle in each page.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,16 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_title, :set_subtitle
   def index
+  end
+
+  private
+
+  def set_title
+    @title = params[:controller].humanize
+  end
+
+  def set_subtitle
+    @subtitle = params[:action].humanize
   end
 end
