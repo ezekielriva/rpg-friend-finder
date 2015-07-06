c5857b8e9235db3bce7118a9110abb35f6b83c8b
Ezekiel <ezerivadeneiral@gmail.com>
2015-07-06 19:26:30 -0300

    Assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
+Rails.application.config.serve_static_assets = true
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
