cb5a9f1a0ba0d6bb1b81f0ab607304bcd9e736df
Ezekiel <ezerivadeneiral@gmail.com>
2015-07-13 19:27:49 -0300

    Add missing confirmation message

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,6 +9,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
     render :edit
   end
 
+  protected
+
+  def after_sign_up_path_for(resource)
+    new_user_session_path
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    new_user_session_path
+  end
+
   private
 
   def user_params
